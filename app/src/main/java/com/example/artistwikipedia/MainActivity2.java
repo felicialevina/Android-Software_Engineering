566c21bcf12d78b75f575744a2461c4633895ef5
Felicia Levina <felicialevina10@gmail.com>
2020-10-04 17:58:43 -0400

    MainActivity2 Java and Layout completed

--- app/src/main/java/com/example/artistwikipedia/MainActivity2.java
+++ app/src/main/java/com/example/artistwikipedia/MainActivity2.java
@@ -2,13 +2,28 @@ package com.example.artistwikipedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView aSong;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        aSong = findViewById(R.id.songsTxt);
+        image = findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        String input = intent.getStringExtra("song");
+        aSong.setText(input);
+
+        int source = intent.getIntExtra("img",0);
+        image.setImageResource(source);
     }
 }
